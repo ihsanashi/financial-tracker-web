aa7817b260e04022ac817fb67f6e18b3db3af529
Ahmad Ihsan <ihsanashi@gmail.com>
2025-01-08 21:30:20 +0800

    chore: update prettier, eslint and ts configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/financial-tracker-web/',
+  // base: '/financial-tracker-web/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@layout': path.resolve(__dirname, './src/layouts'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
     },
   },
 });
