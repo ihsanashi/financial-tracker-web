a566ae39d5f7264179c2129960b00f3430e4581d
Ahmad Ihsan <ihsanashi@gmail.com>
2025-01-11 18:43:07 +0800

    chore: uncomment base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/financial-tracker-web/',
+  base: '/financial-tracker-web/',
   plugins: [react()],
   resolve: {
     alias: {
