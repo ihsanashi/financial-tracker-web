ece6869697c308988445166dd77e006d90ebe9c9
Ahmad Ihsan <ihsanashi@gmail.com>
2025-01-19 21:05:56 +0800

    fix: logout loading state

--- src/routes/(private)/_layout/preferences/route.lazy.tsx
+++ src/routes/(private)/_layout/preferences/route.lazy.tsx
@@ -1,39 +1,54 @@
-import { createLazyFileRoute, redirect } from '@tanstack/react-router'
+import { createLazyFileRoute, useRouter } from '@tanstack/react-router';
+import { Loader } from 'lucide-react';
+import { useState } from 'react';
 
-import { MainContainer } from '@layouts/main-container'
+import { MainContainer } from '@layouts/main-container';
 
-import { supabaseClient } from '@lib/supabase-client'
+import { supabaseClient } from '@lib/supabase-client';
 
-import { useAuthStore } from '@stores/auth'
+import { useAuthStore } from '@stores/auth';
 
-import { Button } from '@ui/button'
+import { Button } from '@ui/button';
 
 export const Route = createLazyFileRoute('/(private)/_layout/preferences')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
   async function handleSignOut() {
-    const { error } = await supabaseClient.auth.signOut()
+    setLoading(true);
+
+    const { error } = await supabaseClient.auth.signOut();
 
     if (error) {
-      console.error('Error from Supabase: ', error)
+      console.error('Error from Supabase: ', error);
     }
 
-    useAuthStore.getState().clearUser()
+    useAuthStore.getState().clearUser();
 
-    redirect({
+    router.navigate({
       to: '/auth/login',
-    })
+    });
+
+    setLoading(false);
   }
 
   return (
     <MainContainer className="p-4">
       <div>Hello "/(private)/_layout/preferences"!</div>
 
-      <Button className="my-4" variant="destructive" onClick={handleSignOut}>
+      <Button
+        className="my-4"
+        disabled={loading}
+        variant="destructive"
+        onClick={handleSignOut}
+      >
+        {loading && <Loader className="animate-spin" />}
         Sign out
       </Button>
     </MainContainer>
-  )
+  );
 }
